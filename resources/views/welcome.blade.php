39d166aa51c0b326747a1f14ddd9adc0971f2750
Ronaldmera <ronaldalme1230@gmail.com>
2025-08-03 12:14:04 -0500

    vista welcome v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,11 @@
 
         <div class="row justify-content-center align-items-center">
             <div class="col-md-6 text-center d-flex flex-column justify-content-center align-items-center">
-                <h2 class="text-white p-4 rounded bg-info w-100 py-5">
-                    ¡Bienvenido a AdminE! <br>
+                <h2 class="bg-light p-4 rounded  w-100 py-5">
+                    ¡Bienvenido a
+                    <span class="text-info">AdminE</span>!<br>
                     Tu panel de control para una gestión eficiente y organizada.
-                    <i class="bi bi-house-door-fill text-dark display-4 d-block mt-2 mt-sm-4"></i>
+                    <i class="bi bi-house-door-fill text-info display-4 d-block mt-2 mt-sm-4"></i>
                 </h2>
             </div>
             <div class="col-md-6">
