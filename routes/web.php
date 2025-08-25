b3b9bd64479077ae51bbcd0bc5c56352e0e89f71
Ronaldmera <ronaldalme1230@gmail.com>
2025-08-24 19:22:32 -0500

    espacio eliminado  entre ruta y metodo

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use App\Http\Controllers\MaterialController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(HomeController::class)->group(function () {
-    Route::get('/',  'index')->name('home.index');
+    Route::get('/','index')->name('home.index');
   });
 
 Route::controller(MaterialController::class)->group(function () {
-    Route::get('materials/missingMaterials',  'showMissingMaterials')->name('material.showMissingMaterials');
-    Route::post('materials/uploadExcel', 'excelInput')->name('material.excelInput');
+    Route::get('materials/missingMaterials','showMissingMaterials')->name('material.showMissingMaterials');
+    Route::post('materials/uploadExcel','excelInput')->name('material.excelInput');
     Route::post('materials/modalData','modalData')->name('material.modalData');
   });
 
