9b404821c9d8b46d0f80a5719cc2cc78b1c40505
Ronald Mera <ronaldalme1230@gmail.com>
2025-09-04 13:25:57 -0500

    materiales faltantes con menu desplegable

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -15,18 +15,25 @@
          </div>
          <ul class="sidebar-menu">
              <li class="menu-header">Opciones</li>
-             {{-- <li class="dropdown">
-                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
-                     <span>Layout</span></a>
+             <li class="dropdown">
+                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
+                         class="bi bi-box text-primary"></i>
+                     <span>Materiales
+                         faltantes</span></a>
                  <ul class="dropdown-menu">
-                     <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-                     <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-                     <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+                     <li><a class="nav-link" href="{{ route('material.showMissingMaterials') }}"><i
+                                 class="bi bi-funnel text-primary"></i><span>Filtrar
+                                 Registros</span></a>
+                     </li>
+                     <li><a class="nav-link" href="#"><i
+                                 class="bi bi-box-arrow-down text-primary"></i><span>Generar Exporte
+                             </span></a>
+                     </li>
                  </ul>
-             </li> --}}
-             <li><a class="nav-link" href="{{ route('material.showMissingMaterials') }}"><i
+             </li>
+             {{-- <li><a class="nav-link" href="{{ route('material.showMissingMaterials') }}"><i
                          class="bi bi-box text-primary"></i> <span>Materiales
-                         faltantes</span></a></li>
+                         faltantes</span></a></li> --}}
              <li><a class="nav-link" href="blank.html"><i class="bi bi-arrow-counterclockwise text-primary"></i>
                      <span>Logistica Inversa
                      </span></a></li>
